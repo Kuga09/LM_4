47a5db9dc9020f31b3bc4e9820b604daba9671a0
Pholl <glal64@tutamail.com>
2025-11-24 14:51:44 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+import numpy as np
+from keras import models, layers
+
+models = models.Sequential([
+    layers.Dense(10, input_shape=(5,))
+
+])
+
+weights, biases = models.layers[0].get_weights()
+
+print(weights.shape)
+print(biases.shape)
+print(weights[:3,:5]) #матрица весов
+print(biases[:10]) #первые 10 сдвигов
